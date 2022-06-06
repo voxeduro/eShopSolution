418f50abfe4ba05e2a15a4fb1f9dbef4e800109b
voxeduro <voxeduro@gmail.com>
2022-06-06 14:16:12 +0700

    create 2 project

---
+++ eShopSolution.Data/Configuration/AppRoleConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.Data.Configuration
+{
+    //public class AppRoleConfiguration : IEntityTypeConfiguration<AppRole>
+    //{
+
+    //}
+}
